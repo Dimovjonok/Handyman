27c5286e7c6eea8da35f8ee305a2debb83ef6a9e
DumDumbIch <dumdumbich@gmail.com>
2022-02-26 15:58:23 +0300

    Test project Blink builtin LED

---
+++ Handyman.ino
@@ -0,0 +1,19 @@
+/*
+Dmitry Lebedev (DumDumbIch) dumdumbich@gmail.com
+Create: 26.02.2022
+Release:
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(2000);                       // wait for a second
+}
