6fbdb96fd783a5a862c6432226bfd79b4764dfdc
DumDumbIch <89682362+Dimovjonok@users.noreply.github.com>
2025-08-02 13:20:32 +0300

    Fix detect

--- app/src/test/java/ru/dumdumbich/smarthome/mobile/handyman/ExampleUnitTest.kt
+++ app/src/test/java/ru/dumdumbich/smarthome/mobile/handyman/ExampleUnitTest.kt
@@ -2,7 +2,7 @@ package ru.dumdumbich.smarthome.mobile.handyman
 
 import org.junit.Test
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 
 /**
  * Example local unit test, which will execute on the development machine (host).
